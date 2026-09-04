8bbdb3f6cd321a0c3dfaf88029173c8634dfa005
kantusgaming <baaphumai096@gmail.com>
2026-09-04 11:43:00 +0530

    Added Employee Entity

---
+++ src/main/java/net/javaguides/ems/entity/Employee.java
@@ -0,0 +1,30 @@
+package net.javaguides.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // @Column(name = "first_name")
+     private String firstName;
+
+    private String lastName;
+    private String email;
+    private Integer age;
+
+
+}
