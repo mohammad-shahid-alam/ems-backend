8d4e0874f374d5e0e854fa272f083da078df9b3d
kantusgaming <baaphumai096@gmail.com>
2026-09-26 17:49:20 +0530

    Added new data into the entity like phoneNumber dataOfJoining salary

---
+++ src/main/java/net/javaguides/ems/entity/Department.java
@@ -0,0 +1,4 @@
+package net.javaguides.ems.entity;
+
+public class Department {
+}
--- src/main/java/net/javaguides/ems/mapper/EmployeeMapper.java
+++ src/main/java/net/javaguides/ems/mapper/EmployeeMapper.java
@@ -9,7 +9,10 @@ public class EmployeeMapper {
                 employee.getId(),
                 employee.getFirstName(),
                 employee.getLastName(),
-                employee.getEmail()
+                employee.getEmail(),
+                employee.getPhoneNumber(),
+                employee.getSalary(),
+                employee.getDateOfJoining()
         );
     }
     public static Employee mapToEmployee(EmployeeDto employeeDto) {
@@ -17,7 +20,10 @@ public class EmployeeMapper {
                 employeeDto.getId(),
                 employeeDto.getFirstName(),
                 employeeDto.getLastName(),
-               employeeDto.getEmail()
+               employeeDto.getEmail(),
+                employeeDto.getPhoneNumber(),
+                employeeDto.getSalary(),
+                employeeDto.getDateOfJoining()
 
         );
     }
